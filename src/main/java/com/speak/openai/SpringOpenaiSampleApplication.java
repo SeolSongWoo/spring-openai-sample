3dddc6c4813993a48ac368155898874576a861ba
SSU <103107905+SeolSongWoo@users.noreply.github.com>
2024-09-30 11:47:06 +0900

    Init

---
+++ src/main/java/com/speak/openai/SpringOpenaiSampleApplication.java
@@ -0,0 +1,13 @@
+package com.speak.openai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOpenaiSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOpenaiSampleApplication.class, args);
+    }
+
+}
